a93741c52c8fcbcfab13d392b06390bb47804c60
ava <ava.cg.lee@gmail.com>
2025-08-08 13:25:44 -0400

    added a new to do view

---
+++ NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,39 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack {
+            Text("Task Title:")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            TextField("Enter task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(15)
+                .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Important?")
+                    .font(.title3)
+                
+            }
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
